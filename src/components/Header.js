e71d96af02b9c96b309116666e6330ccd7b0a554
xognl1124@naver.com <xognl1124@naver.com>
2024-12-13 16:08:29 +0900

    로그인 만들기

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,21 @@
 // src/components/Header.jsx
-import React from "react";
+import React, { useContext } from "react";
 import { AppBar, Box, Toolbar, Button, Container } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import LogoSection from "./LogoSection"; // LogoSection import
+import LogoSection from "./LogoSection";
+import { UserContext } from "../UserContext"; // 추가
 
 const Header = () => {
   const navigate = useNavigate();
+  const { user, setUser } = useContext(UserContext);
 
   const handleLogoClick = () => {
+    navigate("/");
+  };
+
+  const handleLogout = () => {
+    setUser(null);
     navigate("/");
   };
 
@@ -19,9 +26,18 @@ const Header = () => {
           <Box sx={{ marginRight: "auto" }}>
             <LogoSection onClick={handleLogoClick} />
           </Box>
-          <Button component={RouterLink} to="/signin" color="inherit">
-            Login
-          </Button>
+          {user ? (
+            <>
+              <Box sx={{ mr: 2 }}>Welcome, {user.email}</Box>
+              <Button color="inherit" onClick={handleLogout}>
+                Logout
+              </Button>
+            </>
+          ) : (
+            <Button component={RouterLink} to="/signin" color="inherit">
+              Login
+            </Button>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
